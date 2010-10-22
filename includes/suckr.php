b6bd11f5b8ba6387e29d2ca4c3934120ab65d1dd
Meelis Mets <meelis@metz.ee>
2010-10-22 08:02:15 +0000

    verbose typo

--- includes/suckr.php
+++ includes/suckr.php
@@ -77,7 +77,7 @@
                     } else {
                         $ok = "ok";
                         $desc_start = substr($item->get_description(), 0, 5);
-                        if (!strcmp($desc_start, "[…]")) {
+                        if (!strcmp($desc_start, "[...]")) {
                             $ok = "not";
                         }
                         $comment_written = $this->writeComment($title, $link, $base, $date, $content, $f_author_name, $blogger_id);

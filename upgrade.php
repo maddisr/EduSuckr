897922037052d5446bfebfeb75d72e6ee79d700c
Meelis Mets <meelis@metz.ee>
2010-11-26 13:45:14 +0000

    alternate upgrade

--- upgrade.php
+++ upgrade.php
@@ -12,19 +12,19 @@
 		$posts_count = 0;
 		$comments_count = 0;
 
-	    $query = "SELECT * FROM ".DB_PREFIX."educourses";
+	        $select_courses = "SELECT * FROM ".DB_PREFIX."educourses";
 
-		$result = $db->query($query);
-		while($course = mysql_fetch_assoc($result)) {
+		$courses_result = $db->query($select_courses);
+		while($course = mysql_fetch_assoc($courses_result)) {
 			$courses_count++;
-			$message .= 'Course: ' . $course['title'] . '<br />';
+			//$message .= 'Course: ' . $course['title'] . '<br />';
 			// Get gidden posts, old style
-			$query = "SELECT DISTINCT id, c.link as link, title FROM ".DB_PREFIX."posts c LEFT JOIN ".DB_PREFIX."course_rels_posts r ON c.link=r.link WHERE r.course_guid=".$course['course_guid']." AND c.hidden";
-			$result = $db->query($query);
+			$select_posts = "SELECT id, c.link as link, title FROM ".DB_PREFIX."posts c LEFT JOIN ".DB_PREFIX."course_rels_posts r ON c.link=r.link WHERE r.course_guid=".$course['course_guid']." AND c.hidden";
+			$posts_result = $db->query($select_posts);
 			$posts = array();
-			if ($result) {
-				while($res = mysql_fetch_assoc($result)) {
-					$posts[] = $res;
+			if ($posts_result) {
+				while($pst = mysql_fetch_assoc($posts_result)) {
+					$posts[] = $pst;
 				}
 			}
 			foreach ($posts as $post) {
@@ -35,11 +35,19 @@
 				    $message .= '<span style="color:green;">SUCCESS</span><br />';
 					$success_posts_count++;
 				} else {
-					$message .= '<span style="color:red;">FAILED</span><br />';
+                                    $message .= '<span style="color:red;">FAILED </span>';
+                                    $message .= '<span style="color:yellow;">ALTERNATE TRY: </span>';
+                                    $alternate_post_update = 'UPDATE '.DB_PREFIX.'course_rels_posts SET hidden=1 WHERE course_guid='.$course['course_guid'].' AND link="'.$post['link'].'";';
+                                    $post_updated = $db->query($alternate_post_update);
+                                    if ($post_updated) {
+                                        $message .= '<span style="color:green;">SUCCEED</span><br />';
+                                    } else {
+                                        $message .= '<span style="color:red;">FAILED [ '.$alternate_post_update.' ]</span><br />';
+                                    }
 				}
 			}
 			// Get hidden comments, old style
-			$query = "SELECT DISTINCT id, c.link as link, title FROM ".DB_PREFIX."comments c LEFT JOIN ".DB_PREFIX."course_rels_comments r ON c.link=r.link WHERE r.course_guid=".$course['course_guid']." AND c.hidden";
+			$query = "SELECT id, c.link as link, title FROM ".DB_PREFIX."comments c LEFT JOIN ".DB_PREFIX."course_rels_comments r ON c.link=r.link WHERE r.course_guid=".$course['course_guid']." AND c.hidden";
 			$result = $db->query($query);
 			$comments = array();
 			if ($result) {
@@ -55,8 +63,16 @@
 					$message .= '<span style="color:green;">SUCCESS</span><br />';
 					$success_comments_count++;
 				} else {
-					$message .= '<span style="color:red;">FAILED</span><br />';
-				}
+                                    $message .= '<span style="color:red;">FAILED </span>';
+                                    $message .= '<span style="color:yellow;">ALTERNATE TRY: </span>';
+                                    $alternate_comment_update = 'UPDATE '.DB_PREFIX.'course_rels_comments SET hidden=1 WHERE course_guid='.$course['course_guid'].' AND link="'.$comment['link'].'";';
+                                    $comment_updated = $db->query($alternate_comment_update);
+                                    if ($comment_updated) {
+                                        $message .= '<span style="color:green;">SUCCEED</span><br />';
+                                    } else {
+                                        $message .= '<span style="color:red;">FAILED [ '.$alternate_comment_update.' ]</span><br />';
+                                    }
+			        }
 			}
 		}
 		$message .= '<br /><br />All courses: ' . $courses_count . '<br />';

3530e66d3f1f0f59c83c9118f9675cc6823c149b
maddis reitav <maddis.reitav@gmail.com>
2014-04-14 22:53:19 +0300

    Get partcipant_id & course_guid for profile comments

--- includes/db.php
+++ includes/db.php
@@ -162,25 +162,24 @@
             return serialize($comments);
 		}
 		
-		function getParticipantComments($course_guid, $id, $participant_id) {
+		function getParticipantComments($course_guid, $participant_id) {
 			// Check in case base does not have slash in the end
-			;
-			$query = "SELECT DISTINCT c.link AS link, base, title, date, content, author, blogger_id, post_id, post_author, id FROM ".DB_PREFIX."comments c LEFT JOIN ".DB_PREFIX."course_rels_comments r ON c.link=r.link WHERE r.course_guid={$param[0]} AND (c.base='{$backup_base}') AND !r.hidden ORDER BY date DESC";
+			
+			$query = "SELECT DISTINCT c.link AS link, base, title, date, content, author, blogger_id, post_id, post_author, id FROM ".DB_PREFIX."comments c LEFT JOIN ".DB_PREFIX."course_rels_comments r ON c.link=r.link WHERE r.course_guid={$course_guid} AND r.participant_id={$participant_id} AND !r.hidden ORDER BY date DESC";
 			$result = $this->query($query);
 			$comments = array();
-            if($result && mysql_num_rows($result)) {
-                while($comment = mysql_fetch_array($result)) {
-                    if ($comment['blogger_id']) {
-                        if ($fn = $this->getFullnameByBloggerId($comment['blogger_id'])) {
-                            $comment['author'] = $fn;
-                        }
-                    }
-                    $comments []= $comment;
-                }
-            }
-            return serialize($comments);
+			if($result && mysql_num_rows($result)) {
+				while($comment = mysql_fetch_array($result)) {
+					if ($comment['blogger_id']) {
+						if ($fn = $this->getFullnameByBloggerId($comment['blogger_id'])) {
+							$comment['author'] = $fn;
+						}
+					}
+					$comments []= $comment;
+				}
+			}
+			return serialize($comments);
 		}
-
 	   /**
          * Determines if a post is hidden within a course context.
          *
